ded661f52594e00d7c3c2bd82f94b4915a3573cf
Dario Valdespino <dvaldespino00@gmail.com>
2022-01-28 00:44:56 +0100

    Add extension for property accessors.

---
+++ lib/API/src/utils/PropertiesExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace AquaSharp.utils {
+    public static class PropertiesExtensions {
+        public static T? from<T>(this PropertyKey<T> key, Properties properties) {
+            return properties.getProperty(key);
+        }
+    }
+}
--- lib/Standard/src/features/AerationStressFeature.cs
+++ lib/Standard/src/features/AerationStressFeature.cs
@@ -1,19 +1,19 @@
 ﻿using AquaSharp.data;
-
+using AquaSharp.utils;
 
 namespace AquaSharp.features {
     /**<summary>Simulation feature used to calculate the <see cref="AerationStressCoefficient"/>.</summary>*/
     public class AerationStressFeature : SimulationFeature {
         public void step(Properties crop, SimulationData simulation, Properties output) {
             var coefficient = AerationStressCoefficient.calculateCoefficient(
-                simulation.environment.getProperty(RootZoneWater.ActualWaterContent),
-                simulation.environment.getProperty(RootZoneWater.WaterContentAtStressThreshold),
-                simulation.environment.getProperty(RootZoneWater.WaterContentAtSaturation),
-                simulation.environment.getProperty(SimulationEnvironment.AerationDays),
-                crop.getProperty(CropAeration.AerationStuntThreshold),
+                RootZoneWater.ActualWaterContent.from(simulation.environment),
+                RootZoneWater.WaterContentAtStressThreshold.from(simulation.environment),
+                RootZoneWater.WaterContentAtSaturation.from(simulation.environment),
+                SimulationEnvironment.AerationDays.from(simulation.environment),
+                CropAeration.AerationStuntThreshold.from(simulation.environment),
                 out var aerationDays
             );
-            
+
             output.setProperty(SimulationEnvironment.AerationDays, aerationDays);
             output.setProperty(CropAeration.AerationStressCoefficient, coefficient);
         }
