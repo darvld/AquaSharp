b527fd24bc91cfe62043d9f1d8d07c6e74d2f112
Dario Valdespino <dvaldespino00@gmail.com>
2022-01-27 14:09:38 +0100

    Update SimulationFeature API.

--- lib/API/src/engine/SimulationFeature.cs
+++ lib/API/src/engine/SimulationFeature.cs
@@ -2,5 +2,10 @@
     /**
      * <summary>Marker interface implemented by each algorithm (step) in a simulation.</summary>
      */
-    public interface SimulationFeature { }
+    public interface SimulationFeature {
+        /**
+         * <summary>Performs a single-step calculation with the given input and writes the result to the output.</summary>
+         */
+        public void step(Properties crop, SimulationData simulation, Properties output);
+    }
 }
