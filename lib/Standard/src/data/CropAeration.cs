01d5b8e1f05a2774e4b31105c9c09583d3ce4b79
Dario Valdespino <dvaldespino00@gmail.com>
2022-01-28 10:52:39 +0100

    Make `CropAeration` static.

--- lib/Standard/src/data/CropAeration.cs
+++ lib/Standard/src/data/CropAeration.cs
@@ -1,6 +1,6 @@
 ﻿namespace AquaSharp.data {
     /**<summary>A group of property keys related to aeration for a given crop.</summary>*/
-    public static class CropAeration {
+    public static class CropAeration {  
         /**
          * <summary>Number of days before aeration stress stunts crop growth. Corresponds
          * to the <c>Crop.LagAer</c> field in the original implementation.</summary>
